e9cc59868377af9ba9dea079bd82e275f2307e3f
NeyMatiz <ingrid.matiz083@educacionbogota.edu.co>
2025-11-12 15:20:07 -0500

    ajuste setting postgresql

--- unisalud/settings.py
+++ unisalud/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'unisalud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', ''),
         'USER': os.getenv('DB_USER', ''),
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
