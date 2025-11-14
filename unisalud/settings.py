f6503c71e99561635321c9a05cdb59d162a039d7
doruma056 <ricardo.a.arismendy.v@gmail.com>
2025-11-14 15:27:34 -0500

    eliminada parte duplicada
    eliminada parte duplicada

--- unisalud/settings.py
+++ unisalud/settings.py
@@ -91,18 +91,6 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'unisaludBD',  # Nombre de la BD
-    'USER': 'admin',  # Usuario de MySQL
-    'PASSWORD': 'unisaludBD*',  # Cambia por tu contraseña
-    'HOST': '127.0.0.1',  # Servidor de BD
-    'PORT': '3306',  # Puerto de MySQL (por defecto)
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
