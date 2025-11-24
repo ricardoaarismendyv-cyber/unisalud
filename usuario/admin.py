2a00ebe5f61d94c4dc2df3b2f60606211d0ec2e0
NeyMatiz <ingrid.matiz083@educacionbogota.edu.co>
2025-11-24 15:22:38 -0500

    se agrega class usuariosadmin
    se hace con el fin de que el panel de administracion sepa que la contraseña no se debe mostrar plana y así mejorar la seguridad hash

--- usuario/admin.py
+++ usuario/admin.py
@@ -34,18 +34,26 @@ from .models import (
     Turnos,
     Usuarios,
 )
-# Register your models here.
+
+# Se define una clase Admin: UsuariosAdmin para el modelo Usuarios, con el fin de que el panel administracion sepa que la contraseña no debe mostrarse en texto plano
+class UsuariosAdmin(BaseUserAdmin):
+    # Campos que se mostrarán en la lista de usuarios, para mejorar la usabilidad en el panel de administración
+    list_display = ('nombre_usuario', 'email', 'id_rol')
+    # Campos por los que se podrá buscar, para mejorar la usabilidad en el panel de administración
+    search_fields = ('nombre_usuario', 'email')
+    # Campos que no son editables directamente en el admin (la contraseña se maneja aparte)
+    readonly_fields = ()
 
 models_to_register = [
     Afiliacion, AntecedentesPaciente, CentrosMedicos, Ciudad, Consulta, Departamento,
     DiagnosticoPaciente, Enfermedades, Eps, Especialidades, EstadoCivil, EstadoOrden,
     EstratoSocioeconomico, Genero, GrupoRh, Incapacidad, Medicamentos, NivelesAtencion,
     OrdenMedica, Pacientes, ProfesionalSalud, RedesSalud, RegionSalud, ResultadosLaboratorio,
-    Roles, Servicios, TipoIdentificacion, TipoOrden, TiposAfiliacion, Turnos, Usuarios,
+    Roles, Servicios, TipoIdentificacion, TipoOrden, TiposAfiliacion, Turnos,
 ]
-
+#se retira Usuarios de la lista anterior para evitar registrar el modelo dos veces. Esta se registra una vez con la clase de administracion personalizada
 for model in models_to_register:
     admin.site.register(model)
 
 # Registramos el modelo Usuarios usando nuestra clase personalizada
-admin.site.register(Usuarios, UsuariosAdmin)
+admin.site.register(Usuarios, UsuariosAdmin) #inscribe el modelo Usuarios en el admin de Django con la configuración definida en UsuariosAdmin, es decir Quiero administrar mi modelo Usuarios en el panel de administración, y quiero que uses la configuración personalizada definida en la clase UsuariosAdmin para hacerlo
