dd83a9691c4e1bc0da67ce932cc2e6b2ca968035
NeyMatiz <ingrid.matiz083@educacionbogota.edu.co>
2025-11-25 16:00:36 -0500

    Update urls.py

--- prof_salud/urls.py
+++ prof_salud/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('preguntas-frecuentes', views.preguntasfrecuentes_prof_salud, name='preguntas-frecuentes_prof_salud'),
     path('uso-del-sistema', views.usosistema_prof_salud, name='usosistema_prof_salud'),
     path('buzon-sugerencias', views.buzonsugerencias_prof_salud, name='buzonsugerencias_prof_salud'),
-    path('contactanos', views.contactanos_prof_salud, name='contactanos_prof_salud')
+    path('contactanos', views.contactanos_prof_salud, name='contactanos_prof_salud'),
+    path('consulta/<int:consulta_id>/pdf/', views.consulta_pdf, name='consulta_pdf'),
 ]
