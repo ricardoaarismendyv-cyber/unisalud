135dd2b2f3cd650aee1a29ac419326a4cc51db59
NeyMatiz <ingrid.matiz083@educacionbogota.edu.co>
2025-11-26 16:17:30 -0500

    Update settings.py

--- unisalud/settings.py
+++ unisalud/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'usuario.Usuarios'
+# AUTH_USER_MODEL = 'usuario.Usuarios'  # Esta línea se ha comentado o eliminado
 
 
 # Internationalization
