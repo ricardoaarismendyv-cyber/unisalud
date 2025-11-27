0a07ffcf500036b3fbd3eef2c5dd55774c0c0173
NeyMatiz <ingrid.matiz083@educacionbogota.edu.co>
2025-11-27 10:53:26 -0500

    ajustes urls

--- usuario/urls.py
+++ usuario/urls.py
@@ -3,15 +3,24 @@ from . import views
 
 
 urlpatterns = [
-    # Las URLs de login y logout son manejadas por la app 'login'
+    # Inicio y navegación principal
     path('', views.inicio_usuario, name='inicio-usuario'),
-    path('historia-clinica/', views.hcusuario, name='hcusuario'),
-    path('ordenes-medicas', views.omusuario, name='omusuario'),
-    path('ordenes-medicamentos', views.omeusuario, name='omeusuario'),
-    path('turnos', views.turnosusuario, name='turnos-usuario'),
-    path('preguntas-frecuentes', views.preguntasfrecuentes, name='preguntas-frecuentes'),
-    path('uso-del-sistema', views.usosistema, name='usosistema'),
-    path('buzon-sugerencias', views.buzonsugerencias, name='buzonsugerencias'),
-    path('registrarse', views.registro, name='registrarse'),
-    path('contactanos', views.contactanos, name='contactanos'),
+    
+    # Historia clínica
+    path('historia-clinica/', views.hcusuario, name='hcusuario'),    path('registrar-consulta/', views.registrar_consulta, name='registrar-consulta'),    path('registrar-consulta/', views.registrar_consulta, name='registrar-consulta'),
+    
+    # Órdenes médicas (paciente)
+    #path('ordenes-medicas/', views.omusuario, name='omusuario'),
+    #path('ordenes-medicamentos/', views.omeusuario, name='omeusuario'),
+    path('orden/<int:id_orden>/pdf/', views.descargar_pdf_orden_paciente, name='descargar_pdf_orden_paciente'),
+    
+    # Turnos
+    path('turnos/', views.turnosusuario, name='turnos_usuario'),
+    
+    # Páginas públicas
+    path('preguntas-frecuentes/', views.preguntasfrecuentes, name='preguntas-frecuentes'),
+    path('uso-del-sistema/', views.usosistema, name='usosistema'),
+    path('buzon-sugerencias/', views.buzonsugerencias, name='buzon-sugerencias'),
+    path('registrarse/', views.registro, name='registro'),
+    path('contactanos/', views.contactanos, name='contactanos'),
 ]
