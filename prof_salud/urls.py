f79b8b2364729032f173e81ab2dff34f81f604b0
NeyMatiz <ingrid.matiz083@educacionbogota.edu.co>
2025-12-05 16:11:43 -0500

    ajustes de hc pdf

--- prof_salud/urls.py
+++ prof_salud/urls.py
@@ -5,12 +5,14 @@ from . import views
 
 
 urlpatterns = [ 
-    path('inicio_prof_salud/', views.inicio_prof_salud, name='inicio_prof_salud'),
+    path('inicio/', views.inicio_prof_salud, name='inicio_prof_salud'),
     path('paciente/logout_prof_salud/historia_clinica/', views.hc_prof_salud, name='hc_prof_salud'),
     path('historia_clinica/', views.hc_prof_salud, name='hc_prof_salud'),
     path('historia-clinica/diligenciar/', views.diligenciar_hc, name='diligenciar_hc'), #formulario hc
-    path('historia-clinica/pdf/<int:consulta_id>/', views.generar_hc_pdf, name='generar_hc_pdf'),
+    path('historia-clinica/ver-pdf/<int:consulta_id>/', views.ver_hc_pdf, name='ver_hc_pdf'),
+    path('historia-clinica/generar-pdf/<int:consulta_id>/', views.generar_hc_pdf, name='generar_hc_pdf'),
     path('ordenes_medicas/', views.om_prof_salud, name='om_prof_salud'),
+    path('ordenes_medicas/diligenciar/', views.diligenciar_orden_medica, name='diligenciar_orden_medica'),
     path('ordenes_medicamentos/', views.omed_prof_salud, name='omed_prof_salud'),
     path('turnos/', views.consultas_prof_salud, name='consultas_prof_salud'),
     path('preguntas-frecuentes', views.preguntasfrecuentes_prof_salud, name='preguntas-frecuentes_prof_salud'),
