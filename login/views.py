67ccad8335740b9b99dae2c804192f65a3a43204
NeyMatiz <ingrid.matiz083@educacionbogota.edu.co>
2025-12-05 16:13:22 -0500

    ajustes roles

--- login/views.py
+++ login/views.py
@@ -33,16 +33,18 @@ def login_view(request):
                     except ProfesionalSalud.DoesNotExist:
                         messages.warning(request, 'El usuario tiene un rol profesional, pero no un perfil de profesional de salud asociado.')
 
-                # Priorizamos roles de personal de salud
+                # Lógica de redirección por roles
                 if any(rol in roles_profesionales for rol in roles_usuario):
+                    # Prioridad 1: Rol profesional. Si tiene perfil, redirige.
                     if 'id_profesional' in request.session:
-                        return redirect('profesionalS/turnos/')
-                # Si no es profesional, verificamos si es paciente
+                        return redirect('inicio_prof_salud')
                 elif 'paciente' in roles_usuario:
+                    # Prioridad 2: Rol paciente. Si no es profesional pero es paciente, redirige.
                     if 'id_paciente' in request.session:
                         return redirect('inicio-usuario')
-                else:
-                    messages.error(request, 'Rol no reconocido o sin página de inicio definida.')
+                
+                # Si el usuario está autenticado pero no tiene un perfil válido para redirigir, mostramos el error.
+                messages.error(request, 'Rol no reconocido o sin página de inicio definida.')
             else:
                 messages.error(request, 'Nombre de usuario o contraseña incorrectos.')
         except Usuarios.DoesNotExist:
