b40bbf62192cac93eca6c032e71728d0c579e6bd
NeyMatiz <ingrid.matiz083@educacionbogota.edu.co>
2025-12-08 20:40:37 -0500

    agregar ordenes medicas y medicamentos: diligenciar, generar, ver

--- prof_salud/urls.py
+++ prof_salud/urls.py
@@ -6,14 +6,16 @@ from . import views
 
 urlpatterns = [ 
     path('inicio/', views.inicio_prof_salud, name='inicio_prof_salud'),
-    path('paciente/logout_prof_salud/historia_clinica/', views.hc_prof_salud, name='hc_prof_salud'),
-    path('historia_clinica/', views.hc_prof_salud, name='hc_prof_salud'),
+    path('historia_clinica/', views.hc_prof_salud, name='hc_prof_salud'), # Se mantiene una sola ruta con este nombre
     path('historia-clinica/diligenciar/', views.diligenciar_hc, name='diligenciar_hc'), #formulario hc
     path('historia-clinica/ver-pdf/<int:consulta_id>/', views.ver_hc_pdf, name='ver_hc_pdf'),
     path('historia-clinica/generar-pdf/<int:consulta_id>/', views.generar_hc_pdf, name='generar_hc_pdf'),
     path('ordenes_medicas/', views.om_prof_salud, name='om_prof_salud'),
-    path('ordenes_medicas/diligenciar/', views.diligenciar_orden_medica, name='diligenciar_orden_medica'),
+    path('ordenes_medicas/diligenciar_omedica/', views.diligenciar_omedica, name='diligenciar_omedica'),
+    path('ordenes_medicas/ver-pdf/<int:orden_id>/', views.ver_omedica_pdf, name='ver_omedica_pdf'),
+    path('ordenes_medicas/generar-pdf/<int:orden_id>/', views.generar_omedica_pdf, name='generar_omedica_pdf'),
     path('ordenes_medicamentos/', views.omed_prof_salud, name='omed_prof_salud'),
+    path('ordenes_medicamentos/diligenciar/', views.diligenciar_omedicamentos, name='diligenciar_omedicamentos'),
     path('turnos/', views.consultas_prof_salud, name='consultas_prof_salud'),
     path('preguntas-frecuentes', views.preguntasfrecuentes_prof_salud, name='preguntas-frecuentes_prof_salud'),
     path('uso-del-sistema', views.usosistema_prof_salud, name='usosistema_prof_salud'),
